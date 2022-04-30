487d4f8f365cb398f8612aa628b67e494c72b419
Robert Pliszka <robplisz@gmail.com>
2022-04-30 17:13:13 -0400

    Added Potion file which was accidentally deleted. Also wrote tests for Player object

---
+++ __tests__/Player.test.js
@@ -0,0 +1,13 @@
+const { exportAllDeclaration } = require('@babel/types');
+const { default: TestRunner } = require('jest-runner');
+const Player = require('../lib/Player.js');
+
+test('creates a player object', () => {
+    const player = new Player('Dave');
+
+    expect(player.name).toBe('Dave');
+    expect(player.health).toEqual(expect.any(Number));
+    expect(player.strength).toEqual(expect.any(Number));
+    expect(player.agility).toEqual(expect.any(Number));
+
+});
