3f7f1d62ea657fecde2c3d0b3b5e48177e51aa51
Robert Pliszka <robplisz@gmail.com>
2022-04-30 17:35:21 -0400

    Wrote new tests for Player and Potion to check if player had 2 potions. Also used mocks to mock data for the test.

--- __tests__/Player.test.js
+++ __tests__/Player.test.js
@@ -1,6 +1,11 @@
 const { exportAllDeclaration } = require('@babel/types');
 const { default: TestRunner } = require('jest-runner');
+const { JestHook } = require('jest-watcher');
 const Player = require('../lib/Player.js');
+const Potion = require('../lib/Potion.js');
+
+
+jest.mock('../lib/Potion.js');
 
 test('creates a player object', () => {
     const player = new Player('Dave');
@@ -9,5 +14,8 @@ test('creates a player object', () => {
     expect(player.health).toEqual(expect.any(Number));
     expect(player.strength).toEqual(expect.any(Number));
     expect(player.agility).toEqual(expect.any(Number));
+    expect(player.inventory).toEqual(
+        expect.arrayContaining([expect.any(Object)])
+    );
 
 });
